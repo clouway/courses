52677f7935f845313057280d12495a8f76052e83
Valentin Tonkov <vt@tonkov.eu>
2018-03-08 12:45:23 +0200

    Added corrections in the FirstProjects dir projects

--- projects/FirstProjects/FirstProject/src/com/valentin/Main.java
+++ projects/FirstProjects/FirstProject/src/com/valentin/Main.java
@@ -1,132 +1,39 @@
 package com.valentin;
 
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
     static ReverseArray reverseArray=new ReverseArray();
     static ArrClass arrClass=new ArrClass();
     static QuickSort quickSort=new QuickSort();
+    static Math math=new Math();
 
     /**
      *
      * @param args
      */
     public static void main(String[] args) {
-        int[] commonArr={};
+        int[] commonArr = {23,4,54,65,7};
 
-        while (true){
-            try{
-                System.out.println("What do you want to do? \r\n Press a key from 1 to 7 to choose. \r\n 1 for Biggest Common Divisor \n 2 for Greatest Common Divisor");
-                System.out.println("3 for entering a new Array \r\n 4 for min element from the Array");
-                System.out.println("5 for getting the sum of the Array \r\n 6 for quickSorting the array");
-                System.out.println("7 for reversing the array \r\n 8 for EXIT");
-                int request=scanner.nextInt();
-                switch (request) {
-                    case 1:
-                        biggestCommonDiv();
-                        Thread.sleep(1000);
-                        System.out.println("\r\n");
-                        break;
-                    case 2:
-                        greatestCommonDiv();
-                        Thread.sleep(1000);
-                        System.out.println("\r\n");
-                        break;
-                    case 3:
-                        int[] tempArr = enterArr();
-                        commonArr = Arrays.copyOf(tempArr, tempArr.length);
-                        arrClass.printArr(commonArr);
-                        Thread.sleep(1000);
-                        System.out.println("\r\n");
-                        break;
-                    case 4:
-                        System.out.println("The min element is: " + arrClass.getMinElement(commonArr));
-                        Thread.sleep(1000);
-                        System.out.println("\r\n");
-                        break;
-                    case 5:
-                        System.out.println("The sum is: " + arrClass.getSum(commonArr));
-                        Thread.sleep(1000);
-                        System.out.println("\r\n");
-                        break;
-                    case 6:
-                        arrClass.printArr(commonArr);
-                        int[] arrSorted = quickSort.quickSort(commonArr);
-                        System.out.println("The sorted arr is:");
-                        arrClass.printArr(arrSorted);
-                        System.out.println("\r\n");
-                        Thread.sleep(1000);
-                        break;
-                    case 7:
-                        System.out.println("The reversed arr: ");
-                        arrClass.printArr(reverseArray.reverseArray(commonArr));
-                        System.out.println("\r\n");
-                        Thread.sleep(1000);
-                        break;
-                    case 8:
-                        return;
-                }
+        //biggest common divider of 4 and 5
+        math.biggestCommonDiv(4,5);
 
+        //greatest common divider of 4 and 5
+        math.greatestCommonDiv(4,5);
 
-            }catch (Exception e){
-                e.printStackTrace();
-                continue;
-            }
-        }
-    }
+        //Printing the array
+        arrClass.printArr(commonArr);
 
-    public static void biggestCommonDiv() {
-        //Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a: ");
-        int a = scanner.nextInt();
-        System.out.println("Enter b:");
-        int b = scanner.nextInt();
-        while (a!=b){
-            if (a>b){
-                a=a-b;
-            }else
-            {
-                b=b-a;
-            }
-        }
-        System.out.println("The BCD is: " + a);
-    }
+        //The min element
+        arrClass.getMinElement(commonArr);
 
+        //The sum of the elements
+        arrClass.getSum(commonArr);
 
-    /**
-     *
-     */
-    public static void greatestCommonDiv(){
+        //The sorted Array
+        quickSort.quickSort(commonArr);
 
-        System.out.println("Enter a: ");
-        int a = scanner.nextInt();
-        System.out.println("Enter b:");
-        int b = scanner.nextInt();
-        while (b!=0){
-            int c=a%b;
-            a=b;
-            b=c;
+        //The reversed Array
+        reverseArray.reverseArray(commonArr);
 
         }
-        System.out.println("The GCD is: "+a);
     }
-
-    /**
-     *
-     * @return
-     */
-    public static int[] enterArr(){
-        System.out.println("How many numbers?");
-        int n=scanner.nextInt();
-        int arr[]=new int[n];
-        for(int i=0; i<=n-1; i++){
-            System.out.println("Enter a number");
-            arr[i]=(scanner.nextInt());
-        }
-        arrClass.printArr(arr);
-        return arr;
-    }
-}
-//
--- projects/FirstProjects/Sumator/src/com/valentin/Main.java
+++ projects/FirstProjects/Sumator/src/com/valentin/Main.java
@@ -7,9 +7,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        StringToInt stringToInt=new StringToInt();
         Sumator sumator=new Sumator();
-        Scanner scanner=new Scanner(System.in);
+
+
+        BigInteger A=new BigInteger("1234567");
+        BigInteger B=new BigInteger("1233688");
+        BigDecimal D=new BigDecimal("1.1234567");
+        BigDecimal F=new BigDecimal("123.3688");
+
+        System.out.println(sumator.sum(3,5));
+        System.out.println(sumator.sum(3.5,5.32));
+        System.out.println(sumator.sum("Micro","Python"));
+        System.out.println(sumator.sum(D,F));
+        System.out.println(sumator.sum(A,B));
+
+
+
+             /*   Scanner scanner=new Scanner(System.in);
+           StringToInt stringToInt=new StringToInt();
         while (true){
         System.out.print("Enter number: ");
         String input=scanner.next();
@@ -23,21 +38,9 @@ public class Main {
                 System.out.println("The sum is: "+ sumator.sum(a,b)+"\n");
             }
         }
-        }
+        }*/
 
-/*
-        BigInteger A=new BigInteger("1234567");
-        BigInteger B=new BigInteger("1233688");
-        BigDecimal D=new BigDecimal("1.1234567");
-        BigDecimal F=new BigDecimal("123.3688");
 
-        System.out.println(sumator.sum(3,5));
-        System.out.println(sumator.sum(3.5,5.32));
-        System.out.println(sumator.sum("Micro","Python"));
-        System.out.println(sumator.sum(D,F));
-        System.out.println(sumator.sum(A,B));
-
-*/
     }
 }
 //
